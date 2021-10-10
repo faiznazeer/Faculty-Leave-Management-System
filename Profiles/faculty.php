5a98556496fc5d0800005cad6196a122e4900762
faiznazeer <63037011+faiznazeer@users.noreply.github.com>
2021-10-10 23:23:07 +0530

    Add files via upload

---
+++ Profiles/faculty.php
@@ -0,0 +1,19 @@
+<?php
+include("../tool/header.php");
+include("../tool/functions.php");
+?>
+<link rel="stylesheet" href="../css/facultypage.css">
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+
+<div id="divfaculty"></div>
+<script type="text/javascript">
+    var val = "<?php echo $_GET['action']; ?>";
+    $.ajax({
+        type: "POST",
+        url: "../actions.php?action="+val,
+        data: "dept=" + val,
+        success: function(result) {
+            $('#divfaculty').html(result);
+        }
+    }) 
+</script>
